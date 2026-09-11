b40279032abdd8c75d436e982ccad7689f1f2f6a
Ali <aliasgharafzalabadi@gmail.com>
2026-09-11 17:01:30 +0330

    chore: initialize node project

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  { ignores: ['node_modules/', 'coverage/'] },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: { globals: { ...globals.jest } },
+  },
+];
